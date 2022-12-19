01b8fe60a9e18302d90715abd4548941f2fe0d87
baafbass <98693906+baafbass@users.noreply.github.com>
2022-12-19 20:11:59 +0300

    The Configuration of the SQL server is done

--- Movies_E-Tiket/Program.cs
+++ Movies_E-Tiket/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Movies_E_Tiket.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
 //DbContext configuration
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
