c05cb2556e61d38d65ef4641df9419c9841cda92
baafbass <98693906+baafbass@users.noreply.github.com>
2022-12-24 17:24:57 +0300

    IActorService İs Added

---
+++ Movies_E-Tiket/Data/Services/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using Movies_E_Tiket.Models;
+
+namespace Movies_E_Tiket.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+
+        Actor GetById(int id);  
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+
+    }
+}
