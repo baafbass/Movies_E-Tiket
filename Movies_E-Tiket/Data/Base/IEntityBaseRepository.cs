ba4a99fb221e1535b79aa18aaa672c58b3d5bee0
baafbass <98693906+baafbass@users.noreply.github.com>
2022-12-25 09:51:19 +0300

    Generic Repository is introduced

---
+++ Movies_E-Tiket/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Movies_E_Tiket.Models;
+
+namespace Movies_E_Tiket.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T: class,IEntityBase,new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task AddAsync(T enity);
+
+        Task<T> UpdateAsync(int id, T entity);
+
+        Task DeleteAsync(int id);
+    }
+}
